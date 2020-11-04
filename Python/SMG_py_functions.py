47d5e0024d938bd42763e00986367123914b5ed4
Roast-Lord <siloegarcez@gmail.com>
2020-11-04 08:57:23 -0300

    added new files

---
+++ Python/SMG_py_functions.py
@@ -0,0 +1,15 @@
+def Densityair(h): #Função que calcula a densidade do ar dada uma altura "h".
+    
+    p0 = 101.325e3
+    L = 0.0065
+    R = 8.31447
+    M = 0.0289644
+    g = 9.80665
+    T0 = 288.15
+    T = T0 - L*h
+
+    p = p0*((1 - ((L*h)/(T0)))**((g*M)/(R*L)))
+
+    rho = (p*M)/(R*T)
+    
+    return rho
--- Python/Tetooperacional.py
+++ Python/Tetooperacional.py
@@ -1,4 +1,5 @@
 import math
+from SMG_py_functions import Densityair
 
 #Dados da aeronave.
 S = 16.16 
@@ -14,23 +15,6 @@ PM = 111855
 PA_0 = n*PM
 W = W0
 LD_MAX = math.sqrt(((math.pi)*e*RA)/(4*CD0)) 
-
-
-def Densityair(h): #Função que calcula a densidade do ar dada uma altura "h".
-    
-    p0 = 101.325e3
-    L = 0.0065
-    R = 8.31447
-    M = 0.0289644
-    g = 9.80665
-    T0 = 288.15
-    T = T0 - L*h
-
-    p = p0*((1 - ((L*h)/(T0)))**((g*M)/(R*L)))
-
-    rho = (p*M)/(R*T)
-    
-    return rho
 
 #Início do loop de print para os valores no intervalo de [ 18000 m , 24000 m ].
 for height in range(18000,24000+1000,1000):
